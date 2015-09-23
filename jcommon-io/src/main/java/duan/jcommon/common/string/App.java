a1146c10510e0bf3cfdad3696172b423d7dfebf4
cjduan <cjduan@ctrip.com>
2015-09-23 18:14:50 +0800

    init

---
+++ jcommon-io/src/main/java/duan/jcommon/common/string/App.java
@@ -0,0 +1,13 @@
+package duan.jcommon.common.string;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jcommon-io/src/main/java/duan/jcommon/common/time/App.java
@@ -0,0 +1,13 @@
+package duan.jcommon.common.time;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
